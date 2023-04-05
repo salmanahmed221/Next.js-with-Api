03102ddfc2420fd9f9d763a112b96919a968443e
unknown <salmanahmed121221@gmail.com>
2023-04-05 15:47:53 +0500

    Api with next.js and rendering technique

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,67 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+// Step 02 fetching data server static
 
-const inter = Inter({ subsets: ['latin'] })
+// const url = "https://api.quotable.io/random?tags=technology"
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+// async function getData() {
+//   const res = await fetch(url);
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch");
+//   } else {
+//     return res.json();
+//   }
+// }
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
+// export default async function ApiStatic() {
+//   const data = await getData();
+//   return <div>
+//     {data.content}
+//   </div>;
+// }
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+
+
+// Step 03 fetching data server dynamic
+
+// const url = 'https://api.quotable.io/random?tags=technology';
+
+// async function getData() {
+//   const res = await fetch(url, { cache: "no-store" });
+//   if (!res.ok) {
+//     throw new Error("Failed to Fetch");
+//   } else {
+//     return res.json();
+//   }
+// }
+
+
+// export default async function page() {
+//   const data = await getData();
+//   return <div>
+//     {data.content}
+//   </div>;
+// }
+
+
+
+
+
+// Step 04 Fetching data client
+
+// "use client"
+
+// import useSwr from "swr"
+
+// const url = 'https://api.quotable.io/random?tags=technology';
+
+// const fetcher = (url: any) => fetch(url).then((res) => res.json());
+
+// export default function page() {
+//   const { data, isLoading, error } = useSwr(url, fetcher)
+//   if (error) return <div>Failed to fetch</div>
+//   if (isLoading) return <div>loading....</div>
+//   return <div>
+//     {data.content}
+//   </div>;
+// }
